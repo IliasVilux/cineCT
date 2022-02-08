379ad48f37622547a365a084743bf2c4c6af2fb6
gagandeep.dass.7e3 <gagandeep.dass.7e3@itb.cat>
2022-02-08 17:46:01 +0100

    gagandeep.dass.7e3@itb.cat

---
+++ app/Models/Image.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class Image extends Model
+{
+    use HasFactory;
+
+    protected $table = 'profile_images';
+
+    public function user() {
+        return $this->hasOne(User::class);
+    }
+}
