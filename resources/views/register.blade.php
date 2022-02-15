009d5ef29c9e56c33953377cd1e2f5d06169d628
elena.montiel.7e4 <elena.montiel.7e4@itb.cat>
2022-02-15 16:32:53 +0100

    log In Sign Up

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,23 +1,23 @@
 @extends('layout')
 @section('content')
-<section class="section-signin-register vh-100 p-5">
-<div class="p-5">
+<section class="section-signin-register d-flex flex-wrap justify-content-center h-100 p-5">
+<div class="col-12 col-lg-8 p-5">
       <ul class="nav nav-tabs" id="myTab">
         <li class="nav-item">
-          <a href="#signIn" class="nav-link active" data-bs-toggle="tab">Iniciar Sessión</a>
+          <a href="#logIn" class="nav-link active" data-bs-toggle="tab">Log In</a>
         </li>
         <li class="nav-item">
-          <a href="#register" class="nav-link" data-bs-toggle="tab">Registrarse</a>
+          <a href="#signUp" class="nav-link" data-bs-toggle="tab">Sign Up</a>
         </li>
       </ul>
       <div class="tab-content">
-        <div class="tab-pane fade show active" id="signIn">
+        <div class="tab-pane fade show active" id="logIn">
           <div class="card">
-            <div class="card-body p-5">
-            <form class="row g-3 needs-validation d-flex flex-column align-items-center p-5" novalidate>
+            <div class="card-body">
+            <form class="row g-3 needs-validation d-flex flex-column align-items-center p-5 m-2" novalidate>
               <div class="col-12">
                 <label for="validationCustom01" class="form-label">Email</label>
-                <input type="text" class="form-control" id="validationCustom01" placeholder="Mark" required>
+                <input type="text" class="form-control" id="validationCustom01" placeholder="Your email" required>
                 <div class="valid-feedback">
                   Looks good!
                 </div>
@@ -38,29 +38,48 @@
 
             <hr class="my-4">
 
-            <p class="small mb-5 pb-lg-2"><a class="text-dark-50" href="#!">Forgot password?</a></p>
+            <p class="small mb-5"><a class="text-dark-50" href="#!">Forgot password?</a></p>
+            <p class="small text-align-center bg-warning">Don't have an account? <a href="#signIn" class="text-dark-50 fw-bold" data-bs-toggle="tab">Sign Up</a></p>
             </form>
             </div>
           </div>
 
         </div>
-        <div class="tab-pane fade" id="register">
+        <div class="tab-pane fade" id="signUp">
         <div class="card">
-            <div class="card-body p-5">
-            <form class="row g-3 needs-validation d-flex flex-column align-items-center p-5" novalidate>
-              <div class="col-12">
-                <label for="validationCustom01" class="form-label">Email</label>
+            <div class="card-body">
+            <form class="row g-3 needs-validation d-flex flex-column align-items-center p-5 m-2" novalidate>
+              <div class="row p-0">
+              <div class="col-6 mb-2">
+                <label for="validationCustom01" class="form-label">Nombre</label>
                 <input type="text" class="form-control" id="validationCustom01" placeholder="Mark" required>
                 <div class="valid-feedback">
                   Looks good!
                 </div>
               </div>
-              <div class="col-12">
-                <label for="validationCustom01" class="form-label">Password</label>
-                <input type="password" class="form-control" id="validationCustom01" placeholder="Password" required>
+              <div class="col-6 mb-2">
+                <label for="validationCustom01" class="form-label">Email</label>
+                <input type="email" class="form-control" id="validationCustom01" placeholder="youremail@gmail.com" required>
                 <div class="valid-feedback">
                   Looks good!
                 </div>
+              </div>
+              </div>
+              <div class="row p-0">
+              <div class="col-6">
+                <label for="validationCustom01" class="form-label">Password</label>
+                <input type="password" class="form-control" id="validationCustom01" placeholder="New Password" required>
+                <div class="valid-feedback">
+                  Looks good!
+                </div>
+              </div>
+              <div class="col-6">
+                <label for="validationCustom01" class="form-label">Repetir Password</label>
+                <input type="password" class="form-control" id="validationCustom01" placeholder="Repeat Password" required>
+                <div class="valid-feedback">
+                  Looks good!
+                </div>
+              </div>
               </div>
               <div class="form-check d-flex justify-content-start mb-4">
               <input class="form-check-input" type="checkbox" value="" id="form1Example3"/>
@@ -71,7 +90,7 @@
 
             <hr class="my-4">
 
-            <p class="mb-0">Don't have an account? <a href="#!" class="text-dark-50 fw-bold">Sign Up</a></p>
+            <p class="small text-align-center bg-warning">Already have an account? <a href="#logIn" class="text-dark-50 fw-bold" data-bs-toggle="tab">Log In</a></p>
             </form>
             </div>
           </div>
