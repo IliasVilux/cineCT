f0d6e7aef38a3142d9b9ae59a5230ebadc1f1c91
elena.montiel.7e4 <elena.montiel.7e4@itb.cat>
2022-02-16 15:55:31 +0100

    Carousel (WIP)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,9 +7,104 @@
     </a>
 </section>
 <section class="container">
-    <h1>HOLA</h1>
-    <img src="{{url('storage/img/img.png')}}" height="200px">
-    <img src="{{url('storage/img/img2.png')}}" height="200px">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut semper sapien. Aenean augue nibh, mollis eu neque a, ullamcorper dictum massa. Donec iaculis ultrices turpis, sed auctor nibh aliquet et. Morbi tincidunt felis est, quis consectetur leo hendrerit in. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse vel nibh sapien. Nulla ultricies magna elit, vitae feugiat magna scelerisque ut. Vestibulum sit amet fermentum libero. Praesent viverra consectetur hendrerit. Cras bibendum ligula et nunc molestie tristique. Nunc libero tortor, commodo quis nisl quis, rutrum porta eros. Proin auctor, diam vel auctor faucibus, felis dui imperdiet lacus, quis volutpat lorem neque eu tortor. Mauris varius arcu ut ex bibendum, eget consequat orci placerat. Aenean ut tellus efficitur, scelerisque odio fringilla, tincidunt dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum sed eleifend nunc.</p>
+   
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap 5 Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+
+<div class="container">
+
+	<!--Carousel 1-->
+	<div id="multi-item-example" class="carousel slide carousel-equal-heights my-4" data-bs-ride="carousel">
+		<div class="controls-top">
+			<a class="btn btn-primary" href="#multi-item-example" data-bs-slide="prev"><i class="fa fa-chevron-left"></i></a>
+			<a class="btn btn-primary" href="#multi-item-example" data-bs-slide="next"><i class="fa fa-chevron-right"></i></a>
+		</div>
+
+		<div class="carousel-inner" role="listbox">
+
+			<!--Slide 1.1-->
+			<div class="carousel-item active">
+				<div class="row">
+					<div class="col-md-4 d-flex">
+						<div class="card">
+							<img class="card-img-top" src="https://via.placeholder.com/800x600/69c/9fc/?text=Image%201.1" alt="">
+						</div>
+					</div>
+
+					<div class="col-md-4 d-none d-md-flex">
+						<div class="card">
+							<img class="card-img-top" src="https://via.placeholder.com/800x600/69c/9fc/?text=Image%201.2" alt="">
+						</div>
+					</div>
+
+					<div class="col-md-4 d-none d-md-flex">
+						<div class="card">
+							<img class="card-img-top" src="https://via.placeholder.com/800x600/69c/9fc/?text=Image%201.3" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!--Slide 1.2-->
+			<div class="carousel-item">
+				<div class="row">
+					<div class="col-md-4 d-flex">
+						<div class="card">
+							<img class="card-img-top" src="https://via.placeholder.com/800x600/c69/f9c/?text=Image%202.1" alt="">
+						</div>
+					</div>
+
+					<div class="col-md-4 d-none d-md-flex">
+						<div class="card">
+							<img class="card-img-top" src="https://via.placeholder.com/800x600/c69/f9c/?text=Image%202.2" alt="">
+						</div>
+					</div>
+
+					<div class="col-md-4 d-none d-md-flex">
+						<div class="card">
+							<img class="card-img-top" src="https://via.placeholder.com/800x600/c69/f9c/?text=Image%202.3" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!--Slide 1.3-->
+			<div class="carousel-item">
+				<div class="row">
+					<div class="col-md-4 d-flex">
+						<div class="card">
+							<img class="card-img-top" src="https://via.placeholder.com/800x600/9c6/cf9/?text=Image%203.1" alt="">
+						</div>
+					</div>
+
+					<div class="col-md-4 d-none d-md-flex">
+						<div class="card">
+							<img class="card-img-top" src="https://via.placeholder.com/800x600/9c6/cf9/?text=Image%203.2" alt="">
+						</div>
+					</div>
+
+					<div class="col-md-4 d-none d-md-flex">
+						<div class="card">
+							<img class="card-img-top" src="https://via.placeholder.com/800x600/9c6/cf9/?text=Image%203.3" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
 </section>
 @endsection
