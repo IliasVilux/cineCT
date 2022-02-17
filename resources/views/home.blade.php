eee5b1aa85c42beef6c6ffbd9a7341b223f3b314
elena.montiel.7e4 <elena.montiel.7e4@itb.cat>
2022-02-17 15:50:42 +0100

    Pagina Home carousel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,8 @@
 @extends('layoutAll')
 @section('content')
+<head>
+	<link rel="stylesheet" href="storage/css/HomeStyle.css">
+</head>
 <section class="container">
     <h1>MENU</h1>
     <a href="{{ url('/detail') }}" class="btn btn-primary" title="Detail">
@@ -7,104 +10,256 @@
     </a>
 </section>
 <section class="container">
-   
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap 5 Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-<body>
 
+<!-- SERIES -->
+<h5>SERIES</h5>
+    <div class="container d-flex flex-row align-items-center">
+        <a class="btn-floating" href="#series" data-bs-slide="prev"><i
+                class="fa fa-chevron-left fa-2x" aria-hidden="true"></i></a>
 
-<div class="container">
-
-	<!--Carousel 1-->
-	<div id="multi-item-example" class="carousel slide carousel-equal-heights my-4" data-bs-ride="carousel">
-		<div class="controls-top">
-			<a class="btn btn-primary" href="#multi-item-example" data-bs-slide="prev"><i class="fa fa-chevron-left"></i></a>
-			<a class="btn btn-primary" href="#multi-item-example" data-bs-slide="next"><i class="fa fa-chevron-right"></i></a>
-		</div>
-
-		<div class="carousel-inner" role="listbox">
-
-			<!--Slide 1.1-->
-			<div class="carousel-item active">
-				<div class="row">
-					<div class="col-md-4 d-flex">
-						<div class="card">
-							<img class="card-img-top" src="https://via.placeholder.com/800x600/69c/9fc/?text=Image%201.1" alt="">
+        <div id="series" class="carousel slide carousel-equal-heights my-4" data-bs-ride="carousel"
+            data-bs-interval="false">
+			
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                    <div class="row">
+                        <div class="col-3 d-flex justify-content-center">
+							<img class="img-carousel"
+								src="https://via.placeholder.com/800x600/69c/9fc/?text=Image%201.1" alt="">
+                        </div>
+                        <div class="col-3 d-flex justify-content-center">
+							<img class="img-carousel"
+							src="https://via.placeholder.com/800x600/69c/9fc/?text=Image%201.2" alt="">
+                        </div>
+						
+                        <div class="col-3 d-flex justify-content-center">
+							<img class="img-carousel"
+							src="https://via.placeholder.com/800x600/69c/9fc/?text=Image%201.3" alt="">
+                        </div>
+						<div class="col-3 d-flex justify-content-center">
+							<img class="img-carousel"
+								src="https://via.placeholder.com/800x600/69c/9fc/?text=Image%201.4" alt="">
 						</div>
-					</div>
+                    </div>
+                </div>
 
-					<div class="col-md-4 d-none d-md-flex">
-						<div class="card">
-							<img class="card-img-top" src="https://via.placeholder.com/800x600/69c/9fc/?text=Image%201.2" alt="">
+                <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/c69/f9c/?text=Image%202.1" alt="">
+                        </div>
+
+                        <div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/c69/f9c/?text=Image%202.2" alt="">
+                        </div>
+
+                        <div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/c69/f9c/?text=Image%202.3" alt="">
+                        </div>
+						<div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/c69/f9c/?text=Image%202.4" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/9c6/cf9/?text=Image%203.1" alt="">
+                        </div>
+
+                        <div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/9c6/cf9/?text=Image%203.2" alt="">
+                        </div>
+
+                        <div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/9c6/cf9/?text=Image%203.3" alt="">
+                        </div>
+						<div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/9c6/cf9/?text=Image%203.4" alt="">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <a class="btn-floating" href="#series" data-bs-slide="next"><i
+                class="fa fa-chevron-right fa-2x" aria-hidden="true"></i></a>
+    </div>
+
+	<!-- PELICULAS -->
+	<h5>PELICULAS</h5>
+    <div class="container d-flex flex-row align-items-center">
+        <a class="btn-floating" href="#peliculas" data-bs-slide="prev"><i
+                class="fa fa-chevron-left fa-2x" aria-hidden="true"></i></a>
+
+        <div id="peliculas" class="carousel slide carousel-equal-heights my-4" data-bs-ride="carousel"
+            data-bs-interval="false">
+			<div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                    <div class="row">
+                        <div class="col-3 d-flex justify-content-center">
+							<img class="img-carousel"
+								src="https://via.placeholder.com/800x600/69c/9fc/?text=Image%201.1" alt="">
+                        </div>
+                        <div class="col-3 d-flex justify-content-center">
+							<img class="img-carousel"
+							src="https://via.placeholder.com/800x600/69c/9fc/?text=Image%201.2" alt="">
+                        </div>
+						
+                        <div class="col-3 d-flex justify-content-center">
+							<img class="img-carousel"
+							src="https://via.placeholder.com/800x600/69c/9fc/?text=Image%201.3" alt="">
+                        </div>
+						<div class="col-3 d-flex justify-content-center">
+							<img class="img-carousel"
+								src="https://via.placeholder.com/800x600/69c/9fc/?text=Image%201.4" alt="">
 						</div>
-					</div>
+                    </div>
+                </div>
 
-					<div class="col-md-4 d-none d-md-flex">
-						<div class="card">
-							<img class="card-img-top" src="https://via.placeholder.com/800x600/69c/9fc/?text=Image%201.3" alt="">
+                <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/c69/f9c/?text=Image%202.1" alt="">
+                        </div>
+
+                        <div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/c69/f9c/?text=Image%202.2" alt="">
+                        </div>
+
+                        <div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/c69/f9c/?text=Image%202.3" alt="">
+                        </div>
+						<div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/c69/f9c/?text=Image%202.4" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/9c6/cf9/?text=Image%203.1" alt="">
+                        </div>
+
+                        <div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/9c6/cf9/?text=Image%203.2" alt="">
+                        </div>
+
+                        <div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/9c6/cf9/?text=Image%203.3" alt="">
+                        </div>
+						<div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/9c6/cf9/?text=Image%203.4" alt="">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <a class="btn-floating" href="#peliculas" data-bs-slide="next"><i
+                class="fa fa-chevron-right fa-2x" aria-hidden="true"></i></a>
+    </div>
+
+	<!-- ANIMES -->
+	<h5>ANIMES</h5>
+    <div class="container d-flex flex-row align-items-center">
+
+        <a class="btn-floating" href="#animes" data-bs-slide="prev"><i
+                class="fa fa-chevron-left fa-2x" aria-hidden="true"></i></a>
+
+        <div id="animes" class="carousel slide carousel-equal-heights my-4" data-bs-ride="carousel"
+            data-bs-interval="false">
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                    <div class="row">
+                        <div class="col-3 d-flex justify-content-center">
+							<img class="img-carousel"
+								src="https://via.placeholder.com/800x600/69c/9fc/?text=Image%201.1" alt="">
+                        </div>
+                        <div class="col-3 d-flex justify-content-center">
+							<img class="img-carousel"
+							src="https://via.placeholder.com/800x600/69c/9fc/?text=Image%201.2" alt="">
+                        </div>
+						
+                        <div class="col-3 d-flex justify-content-center">
+							<img class="img-carousel"
+							src="https://via.placeholder.com/800x600/69c/9fc/?text=Image%201.3" alt="">
+                        </div>
+						<div class="col-3 d-flex justify-content-center">
+							<img class="img-carousel"
+								src="https://via.placeholder.com/800x600/69c/9fc/?text=Image%201.4" alt="">
 						</div>
-					</div>
-				</div>
-			</div>
+                    </div>
+                </div>
 
-			<!--Slide 1.2-->
-			<div class="carousel-item">
-				<div class="row">
-					<div class="col-md-4 d-flex">
-						<div class="card">
-							<img class="card-img-top" src="https://via.placeholder.com/800x600/c69/f9c/?text=Image%202.1" alt="">
-						</div>
-					</div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/c69/f9c/?text=Image%202.1" alt="">
+                        </div>
 
-					<div class="col-md-4 d-none d-md-flex">
-						<div class="card">
-							<img class="card-img-top" src="https://via.placeholder.com/800x600/c69/f9c/?text=Image%202.2" alt="">
-						</div>
-					</div>
+                        <div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/c69/f9c/?text=Image%202.2" alt="">
+                        </div>
 
-					<div class="col-md-4 d-none d-md-flex">
-						<div class="card">
-							<img class="card-img-top" src="https://via.placeholder.com/800x600/c69/f9c/?text=Image%202.3" alt="">
-						</div>
-					</div>
-				</div>
-			</div>
+                        <div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/c69/f9c/?text=Image%202.3" alt="">
+                        </div>
+						<div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/c69/f9c/?text=Image%202.4" alt="">
+                        </div>
+                    </div>
+                </div>
 
-			<!--Slide 1.3-->
-			<div class="carousel-item">
-				<div class="row">
-					<div class="col-md-4 d-flex">
-						<div class="card">
-							<img class="card-img-top" src="https://via.placeholder.com/800x600/9c6/cf9/?text=Image%203.1" alt="">
-						</div>
-					</div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/9c6/cf9/?text=Image%203.1" alt="">
+                        </div>
 
-					<div class="col-md-4 d-none d-md-flex">
-						<div class="card">
-							<img class="card-img-top" src="https://via.placeholder.com/800x600/9c6/cf9/?text=Image%203.2" alt="">
-						</div>
-					</div>
+                        <div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/9c6/cf9/?text=Image%203.2" alt="">
+                        </div>
 
-					<div class="col-md-4 d-none d-md-flex">
-						<div class="card">
-							<img class="card-img-top" src="https://via.placeholder.com/800x600/9c6/cf9/?text=Image%203.3" alt="">
-						</div>
-					</div>
-				</div>
-			</div>
+                        <div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/9c6/cf9/?text=Image%203.3" alt="">
+                        </div>
+						<div class="col-3 d-flex justify-content-center">
+                                <img class="img-carousel"
+                                    src="https://via.placeholder.com/800x600/9c6/cf9/?text=Image%203.4" alt="">
+                        </div>
+                    </div>
+                </div>
 
-		</div>
-	</div>
-</div>
-
-</body>
-</html>
+            </div>
+        </div>
+        <a class="btn-floating" href="#animes" data-bs-slide="next"><i
+                class="fa fa-chevron-right fa-2x" aria-hidden="true"></i></a>
+    </div>
 </section>
 @endsection
