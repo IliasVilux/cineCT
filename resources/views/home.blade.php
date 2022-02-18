ccfeb2b815f9fdb18085429c3d1d4a2472180e57
elena.montiel.7e4 <elena.montiel.7e4@itb.cat>
2022-02-18 16:18:08 +0100

    Home Carousel full width

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,33 @@
 <head>
 	<link rel="stylesheet" href="storage/css/home.css">
 </head>
-<section class="container">
-    <h1>MENU</h1>
-    <a href="{{ url('/detail') }}" class="btn btn-primary" title="Detail">
-        Detail
-    </a>
+<section class="section-container">
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item full active">
+        <img class="full-img" src="{{url('storage/img/img.png')}}" alt="">
+    </div>
+    <div class="carousel-item full">
+    <img class="full-img" src="{{url('storage/img/img2.png')}}" alt="">
+    </div>
+    <div class="carousel-item full">
+    <img class="full-img" src="{{url('storage/img/img3.png')}}" alt="">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 </section>
 <section class="container">
 
