2227d532d3bb955342a139156ca73e60aa6758f1
ilias.otsman.7e4 <ilias.otsman.7e4@itb.cat>
2022-02-21 16:50:53 +0100

    series v1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,7 +30,15 @@ class DatabaseSeeder extends Seeder
         $tmp = SerieController::store();
         foreach ($tmp as $tmp2){
             DB::table('series')->insert([
+                'image_api_id' => $tmp2->{'id'},
                 'name' => $tmp2->{'name'}, 
+                'description' => $tmp2->{'overview'},
+                'genre_id' => $tmp2->{'genres'}[0]->{'name'},
+                'adult_content' => $tmp2->{'adult'},
+                'release_date' => $tmp2->{'first_air_date'},
+                'seasons' => $tmp2->{'number_of_seasons'},
+                'total_episodes' => $tmp2->{'number_of_episodes'},
+                'puntuation' => $tmp2->{'vote_average'},
                 'created_at' => now(),
                 'updated_at' => now()
             ]);
