eb947a8045970c959593d730030b62e985bf31e5
elena <elena.montiel.7e4@itb.cat>
2022-03-02 15:51:26 +0100

    Header fixed y onscroll fixed-top

--- resources/views/headerFooter.blade.php
+++ resources/views/headerFooter.blade.php
@@ -11,7 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- FontAwesome -->
-    <script src="https://kit.fontawesome.com/1f7457abdb.js"></script>
+    <script src="../storage/js/header.js"></script>
     <title>Cinect</title>
     <link rel="stylesheet" href="../storage/css/general.css">
     <link rel="stylesheet" href="../storage/css/header.css">
@@ -20,7 +20,7 @@
 
 <body>
     <!-- START HEADER -->
-    <header class="navbar-dark fixed-top">
+    <header class="navbar-dark fixed-top" id="navbar_top">
         <!-- START NAVBAR -->
         <nav class="navbar navbar-expand-xs">
             <div class="container-fluid">
