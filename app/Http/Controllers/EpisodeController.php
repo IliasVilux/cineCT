12f54dc5462afbc94a1f24e401279692f3121cf1
gagandeep.dass.7e3 <gagandeep.dass.7e3@itb.cat>
2022-03-08 17:16:36 +0100

    fetch all series-episodes from season 1

--- app/Http/Controllers/EpisodeController.php
+++ app/Http/Controllers/EpisodeController.php
@@ -8,8 +8,9 @@ use Illuminate\Support\Facades\Http;
 
 class EpisodeController extends Controller
 {
-    
-    public static function store(){
+
+    public static function store()
+    {
         $anime = new AnimeController;
         $animes = $anime::index();
 
@@ -20,20 +21,20 @@ class EpisodeController extends Controller
         $allEpisodes = array();
 
         $contID = 0;
-        foreach ($animes as $idLink){
+        foreach ($animes as $idLink) {
             $episodeApi = Http::get('https://api.jikan.moe/v4/anime/' . $idLink->original_id . '/episodes');
             array_push($apiLinks, $episodeApi);
             array_push($id_animes, $idLink->original_id);
             array_push($duration_animes, $idLink->duration);
             sleep(4);
         }
-        
-        foreach($apiLinks as $link){
+
+        foreach ($apiLinks as $link) {
             $episodeJson = json_decode($link);
-            if (isset($episodeJson) && !empty($episodeJson->{'data'})){
+            if (isset($episodeJson) && !empty($episodeJson->{'data'})) {
                 $contEpisodes = count($episodeJson->{'data'});
-                for ($cont=0; $cont<=$contEpisodes; $cont++){
-                    if (!empty($episodeJson->{'data'}[$cont])){
+                for ($cont = 0; $cont <= $contEpisodes; $cont++) {
+                    if (!empty($episodeJson->{'data'}[$cont])) {
                         array_push($allEpisodes, $episodeJson->{'data'}[$cont]);
                     }
                 }
@@ -68,50 +69,45 @@ class EpisodeController extends Controller
         $series = $serie::fetchAllSeries();
 
         $allseriesFromApiWithFiltred = new SerieController();
-        $seriesWithFilter = $allseriesFromApiWithFiltred::store();      
+        $seriesWithFilter = $allseriesFromApiWithFiltred::store();
 
         $allSerieEpisodes = array();
 
-        foreach($series as $serie){
-            $episodeSerieApi = Http::get('https://api.themoviedb.org/3/tv/'.$serie->original_id.'/season/1?api_key=9d981b068284aca44fb7530bdd218c30&language=en-EN');
-            if($serie->original_id >= 10)
-            {
+        foreach ($series as $serie) {
+            $episodeSerieApi = Http::get('https://api.themoviedb.org/3/tv/' . $serie->original_id . '/season/1?api_key=9d981b068284aca44fb7530bdd218c30&language=en-EN');
+            if ($serie->original_id >= 10) {
                 break;
             }
             array_push($allSerieEpisodes, $episodeSerieApi);
         }
 
-        
-        foreach($allSerieEpisodes as $serieEpisode)
-        {
+
+        foreach ($allSerieEpisodes as $serieEpisode) {
             $episodeJson = json_decode($serieEpisode);
+            $serieEpisode = $episodeJson->{'episodes'};
             $episodeCount = count($episodeJson->{'episodes'});
-            if(isset($episodeJson->{'episodes'}) && $episodeCount > 0){
-                foreach($seriesWithFilter as $serieManipulated){
-                    if(isset($serieManipulated->{'last_episode_to_air'})){
+            $last_episode = array_pop($serieEpisode);
+            if (isset($episodeJson->{'episodes'}) && $serieEpisode > 0) {
+                foreach ($seriesWithFilter as $serieManipulated) {
 
-                        if($serieManipulated->{'last_episode_to_air'}->{'name'} == end($episodeJson->{'episodes'}->{'name'})){
+                    if (isset($serieManipulated->{'last_episode_to_air'})) {
 
+                        if ($serieManipulated->{'last_episode_to_air'}->{'id'} == $last_episode->{'id'} && $serieManipulated->{'last_episode_to_air'}->{'name'} == $last_episode->{'name'}) {
+
+                            foreach ($series as $serie) {
+                                echo "Episodios de: " . $serie->name . '<br>';
+                                echo "--------------------<br>";
+
+                                for ($i = 0; $i < $episodeCount; $i++) {
+                                    echo 'Episodio ' . $episodeJson->{'episodes'}[$i]->{'episode_number'} . ' - ' . $episodeJson->{'episodes'}[$i]->{'name'} . '<br>';
+                                }
+                                echo "-------------------------------<br>";
+                            }
+                            //echo $serieManipulated->{'last_episode_to_air'}->{'name'};
                         }
-                        echo $serieManipulated->{'last_episode_to_air'}->{'name'};
-                        
-                        /*
-                        for ($i=0; $i <$episodeCount ; $i++) { 
-                            echo $episodeJson->{'episodes'}[$i]->{'episode_number'}.' - '.$episodeJson->{'episodes'}[$i]->{'name'}.'<br>';
-                        }
-                        echo "-------------------------------<br>";
-                        */
-
                     }
-                   
                 }
-
-                
             }
         }
-
-
-
     }
-    
 }
