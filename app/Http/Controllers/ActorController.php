b1a9e83666ce250f9ddf19c62610cab926edbde6
ilias.otsman.7e4 <ilias.otsman.7e4@itb.cat>
2022-03-10 16:18:05 +0100

    merge

---
+++ app/Http/Controllers/ActorController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Film;
+use App\Models\Serie;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class Actorcontroller extends Controller
+{
+    public static function filmActors()
+    {
+
+
+        $film = new FilmController();
+        $films = $film::store();
+
+        $dbFilms = Film::get();
+
+        /*
+        foreach($dbFilms as $dbFilm){
+            echo 'ID:' .$dbFilm->id .'- API ID: '.$dbFilm->original_id .'<br>';
+        }
+
+        die();
+        */
+
+        $apiLinks = array();
+        $allActors = array();
+
+        foreach($films as $film) {
+            $actorApi = Http::get('https://api.themoviedb.org/3/movie/' . $film->id . '/credits?api_key=9d981b068284aca44fb7530bdd218c30&language=en-EN');
+            array_push($apiLinks, $actorApi);
+        };
+
+
+        foreach ($apiLinks as $link) {
+            $actorJson = json_decode($link);
+            $actors = $actorJson;
+            if (isset($actors->{'id'}) && !empty($actors->{'cast'})) {
+                foreach($dbFilms as $dbFilm){
+                    if($actors->{'id'} == $dbFilm->original_id){
+                        $actors->{'id'} = $dbFilm->id;
+                    }
+                }
+                array_push($allActors, $actors);
+            }
+        }
+
+        
+        
+        /*
+        $limitActor = 2;
+        $totalActores = count($allActors);
+        var_dump($totalActores);
+        die();
+      
+        echo 'MOVIE_ID - '.$actor->{'id'}.'<br>';
+        for ($i=0; $i <$limitActor ; $i++) { 
+        }
+
+        foreach($allActors as $actor){
+            echo $actor->{'cast'}[1]->{'name'} .'<br>';
+            echo '-----------------------<br>';
+            
+        }
+        */
+          
+          
+        return $allActors; 
+    }
+
+    
+    public static function serieActors() 
+    {
+        $serie = new SerieController();
+        $series = $serie::store();
+
+        $apiLinks = array();
+        $allActors = array();
+
+        $dbSeries = Serie::get();
+
+        foreach($series as $serie) {
+            $actorApi = Http::get('https://api.themoviedb.org/3/tv/'.$serie->id.'/credits?api_key=9d981b068284aca44fb7530bdd218c30&language=en-EN');
+            array_push($apiLinks, $actorApi);
+        };
+
+        foreach ($apiLinks as $link) {
+            $actorJson = json_decode($link);
+            $actors = $actorJson;
+            if (isset($actors->{'id'}) && !empty($actors->{'cast'})) {
+                foreach($dbSeries as $dbSerie){
+                    if($actors->{'id'} == $dbSerie->original_id){
+                        $actors->{'id'} = $dbSerie->id;
+                    }
+                }
+                array_push($allActors, $actors);
+            }
+        }
+
+        /*
+        foreach($allActors as $serieActor) {
+            if(count($serieActor->{'cast'}) > 2){
+                for ($i=0; $i <2 ; $i++) { 
+                    echo $serieActor->{'id'} . ' - ' . $serieActor->{'cast'}[$i]->{'name'} .'<br>';   
+                }
+            }else{
+                echo $serieActor->{'id'} . ' - ' . $serieActor->{'cast'}[0]->{'name'} .'<br>';
+            }
+        }
+        */
+
+        return $allActors;
+    }
+}
--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -8,44 +8,86 @@ use Illuminate\Support\Facades\Http;
 class CharacterController extends Controller
 {
     public static function store(){
-
-        
-        $anime = new AnimeController();
+        $anime = new AnimeController;
         $animes = $anime::index();
-        
+
         $contador = 1;
         $apiLinks = array();
         $allCharacters = array();
-        //$characterAnimes = array();
+        $idCharacters = array();
+        $idAnimes = array();
 
         do{
-            $chracterApi = Http::get('https://api.jikan.moe/v4/characters/' . $contador .'/anime');
-            array_push($apiLinks, $chracterApi);
-            $contador++;
+            $chracterApi = Http::get('https://api.jikan.moe/v4/characters/' . $contador . '/anime');
+            /* array_push($apiLinks, $chracterApi); */
             sleep(4);
-        }while($contador < 5);
-        
-        //$characterContador = 0;
-        foreach($apiLinks as $link){
-            $characterJson = json_decode($link);
-            $countCharacter = count($characterJson->{'data'});
-            if(isset($characterJson->{'data'}) && count($characterJson->{'data'}) > 0){
-                for ($i=0; $i <$countCharacter ; $i++) { 
-                    foreach($animes as $anime){
-                        if($characterJson->{'data'}[$i]->{'anime'}->{'title'} == $anime->name){
-                            array_push($allCharacters, $characterJson);
-                            break;
+            $characterJson = json_decode($chracterApi);
+            if (isset($characterJson->{'data'}) && !empty($characterJson->{'data'})){
+                foreach ($characterJson->{'data'} as $key) {
+                    $find = false;
+                    $title = $key->{'anime'}->{'title'};
+                    foreach ($animes as $anime) {
+                        if($title == $anime->name){
+                            if (!$find){
+                                /* array_push($allCharacters, $key); */
+                                array_push($idAnimes, $anime->original_id);
+                                array_push($idCharacters, $contador);
+                                $find = true;
+                            }
                         }
-                    }                    
+                    }
                 }
-            }            
-        }
+            }
+            $contador++;
+        } while($contador < 100);
 
-        foreach($allCharacters as $character){
-            echo $character->{'data'}->{'anime'}->{'title'};
-            die();
+        $contAnimeId = 0;
+        foreach ($idCharacters as $idCharacter) {
+            $specificCharaccterApi = Http::get('https://api.jikan.moe/v4/characters/' . $idCharacter);
+            $spacificCharacterJson = json_decode($specificCharaccterApi);
+            if(isset($spacificCharacterJson->{'data'}->{'name'})){
+                echo $spacificCharacterJson->{'data'}->{'name'} . "<br>";
+            }
+            if(isset($spacificCharacterJson->{'data'}->{'nicknames'}) && !empty($spacificCharacterJson->{'data'}->{'nicknames'})){
+                echo $spacificCharacterJson->{'data'}->{'nicknames'}[0] . "<br>";
+            }
+            if(isset($spacificCharacterJson->{'data'}->{'about'})){
+                echo $spacificCharacterJson->{'data'}->{'about'} . "<br>";
+            }
+            echo $idAnimes[$contAnimeId];
+            $contAnimeId++;
+            echo "<br><br>";
+            /* if (isset($characterJson->{'data'}) && !empty($characterJson->{'data'})){
+                if (isset($characterJson->{'data'}->{'images'}->{'webp'}) && !empty($characterJson->{'data'}->{'images'}->{'webp'})){
+                    echo "<img src=" . $characterJson->{'data'}->{'images'}->{'webp'}->{'image_url'} . ">";
+                }
+            } */
+            sleep(4);
         }
+        die();
 
-        //return $allCharacters;
+        /* foreach($apiLinks as $link){
+            $characterJson = json_decode($link);
+            if (isset($characterJson->{'data'}) && !empty($characterJson->{'data'})){
+                foreach ($characterJson->{'data'} as $key) {
+                    $find = false;
+                    $title = $key->{'anime'}->{'title'};
+                    foreach ($animes as $anime) {
+                        if($title == $anime->name){
+                            if (!$find){
+                                array_push($allCharacters, $key);
+                                $find = true;
+                            }
+                        }
+                    }
+                }
+            }
+        } */
+
+        /* foreach ($allCharacters as $asd) {
+            echo $asd->{'anime'}->{'title'} . "<br>";
+        } */
+        die;
+        return $allCharacters;
     }
 }
