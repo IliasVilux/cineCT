42e9537f64483c1a76a55e9b4263edc2773b2ee5
judDam <judith.martinez.7e2@itb.cat>
2022-03-28 17:22:29 +0200

    Ajustes profile

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -3,27 +3,30 @@
 
 <head>
     <link rel="stylesheet" href="{{asset('css/general.css')}}">
-    <link rel="stylesheet" href="{{asset('css/profile.css')}}">
 </head>
 <section class="container">
-    <h1>MI PERFIL</h1>
+    <h1 class="mt-4">MI PERFIL</h1>
     <div class="d-flex justify-content-center">
         <a href="{{asset('/profileImg')}}">
-            <img class="img-carousel" src="{{asset('img/Dexter.jpg')}}">
+            <img class="img-carousel" src="{{asset('img/NoImg.jpg')}}" width="200px">
         </a>
     </div>
+    <a href="{{asset('/profileImg')}}" class="col-12 d-flex justify-content-center" >
+        <button class="btn btn-primary mt-3 col-3">Cambiar imagen</button>
+    </a>
 
-    <form class="row g-3 my-4 mx-5" id="save" action="{{ route('user.update') }}" method="POST">
+
+    <form class="row g-3 my-4" id="save" action="{{ route('user.update') }}" method="POST">
         @csrf
-        <div class="col-12 col-sm-6 px-4">
+        <div class="col-12 col-sm-6 p-0 m-0 mb-3 pe-2">
             <label for="username" class="form-label">Nombre de usuario</label>
             <input type="text" class="form-control" name="username" id="username" value="{{ Auth::user()->nick }}">
         </div>
-        <div class="col-12 col-sm-6 px-4">
+        <div class="col-12 col-sm-6 p-0 m-0 mb-3 ps-2">
             <label for="realName" class="form-label">Nombre Completo</label>
             <input type="text" class="form-control" name="realName" id="realName" value="{{ Auth::user()->name.' '.Auth::user()->surname}}" disabled>
         </div>
-        <div class="col-12 col-sm-6 px-4">
+        <div class="col-12 col-sm-6 p-0 m-0 pe-2">
         <label class="form-label">Idioma</label>
         <p id="lang" style="display: none;">{{ Auth::user()->lang }}</p>
         <select class="form-select" name="language">
@@ -33,11 +36,11 @@
             <option id="en" value="en">Inglés</option>
         </select>
         </div>
-        <div class="col-12 col-sm-6 p-4">
+        <div class="col-12 col-sm-6 m-0 ps-2" style="padding-top: 25px;">
             <a href="{{ route('change.password') }}" type="button" class="btn btn-light col-12 my-2">Cambiar contraseña</a>
         </div>
-        <div class="col-12 px-4 d-flex justify-content-center">
-            <button type="submit" class="btn btn-primary col-12 col-sm-6">Guardar</button>
+        <div class="col-12 d-flex justify-content-center">
+            <button type="submit" class="btn btn-primary col-3">Guardar</button>
         </div>
     </form>
 </section>
