29db0ff2f45104000d3d924c3361ece33f326943
judDam <judith.martinez.7e2@itb.cat>
2022-04-05 17:09:14 +0200

    Register/login/warnings lang

---
+++ resources/lang/es/warnings.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'email_wrong' => 'El email no es válido o ya se está utilizando.',
+    'pass_wrong' => 'La contraseña debe tener por lo menos 6 carácteres.',
+    'user_no_account' => 'El nombre de usuario no pertenece a ninguna cuenta registrada.',
+    'name_wrong' => 'El nombre debe tener por lo menos 3 carácteres.',
+    'last_name_wrong' => 'El apellido debe tener por lo menos 4 carácteres.',
+    'user_used' => 'Este nombre de usuario ya existe o tiene más de 15 carácteres.',
+    'email_used' => 'Este email se está utilizando en otra cuenta.',
+    'same_pass' => 'No es la misma contraseña.',
+
+];
