aa2c892a7208b64cc4250f9c8d93c7811ff05eeb
Gagandeep Dass <gagandeepdass@berrly.com>
2022-04-17 17:37:51 +0200

    fixing 'activity' view

--- resources/views/activity/activity.blade.php
+++ resources/views/activity/activity.blade.php
@@ -14,43 +14,34 @@
     @else
         <h2 class="text-center p-3 mb-4 mt-2">{{ trans('titles.activity') }}</h2>
         @foreach ($likes as $like)
-                <div class="user-activity mb-2">
-                    @if ($like->review->film)
-                        <a href="{{ route('film.films', ['id' => $like->review->film->id]) }}">
-                    @elseif($like->review->serie)
-                        <a href="{{ route('serie.series', ['id' => $like->review->serie->id]) }}">
-                    @else
-                        <a href="{{ route('anime.animes', ['id' => $like->review->anime->id]) }}">
-                    @endif
-                    <div class="d-flex mb-2">
-                        <div>
-                            <img class="rounded-circle shadow-1-strong me-3"
-                                src="https://i.pinimg.com/originals/bf/a5/3b/bfa53b2488eb224410ac1edfbecb2a34.png" alt="13"
-                                width="65" height="65" />
-                        </div>
-                        <div>
-                            <div class="user-activity__information">
-                                <p class="mb-1"><strong>{{ $like->user->nick }}</strong> liked your review: <span class="text-secondary">{{$like->review->description}}</span>
-                                    <span class="d-block text-secondary ml-2">{{ \DateTimeFormat::timeFilter($like->created_at) }}</span>
-                                </p>
-                                {{-- 
-                                <div class="review-detail">
-                                    @if ($like->review->film)
-                                        <div><img src="{{ $like->review->film->poster_path }}" class="review-image-information" alt="Img"></div>
-                                    @elseif($like->review->serie)
-                                        <div><img src="{{ $like->review->serie->poster_path }}" class="review-image-information" alt="Img"></div>
-                                    @else
-                                        <div><img src="{{ $like->review->anime->poster_path }}" class="review-image-information" alt="Img"></div>
-                                    @endif  
-                                </div>
-                            --}}
-                                </div>
-
-                            </div>
-                        </div>
+            <div class="user-activity mb-2">
+                @if ($like->review->film)
+                    <a href="{{ route('film.films', ['id' => $like->review->film->id]) }}">
+                @elseif($like->review->serie)
+                    <a href="{{ route('serie.series', ['id' => $like->review->serie->id]) }}">
+                @else
+                    <a href="{{ route('anime.animes', ['id' => $like->review->anime->id]) }}">
+                @endif
+                <div class="d-flex mb-2">
+                    <div>
+                        <img class="rounded-circle shadow-1-strong me-3"
+                            src="https://i.pinimg.com/originals/bf/a5/3b/bfa53b2488eb224410ac1edfbecb2a34.png" alt="13"
+                            width="65" height="65" />
                     </div>
-                    </a>
+                    <div>
+                        <div class="user-activity__information">
+                            <p class="mb-1"><strong>{{ $like->user->nick }}</strong> liked your comment: <span
+                                    class="text-secondary">{{ $like->review->description }}</span>
+                                <span
+                                    class="d-block ml-2 opacity-75">{{ \DateTimeFormat::timeFilter($like->created_at) }}</span>
+                            </p>
+                        </div>
+
+                    </div>
                 </div>
+            </div>
+            </a>
+            </div>
         @endforeach
         {{-- Pagination --}}
         <div class="d-flex justify-content-center my-4">
