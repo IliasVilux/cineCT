13f8d92a1f5cb9159f8b69467b66f4ec9faec04a
Gagandeep Dass <gagandeepdass@berrly.com>
2022-05-02 19:19:52 +0200

    set Model getters

--- app/Models/Serie.php
+++ app/Models/Serie.php
@@ -13,6 +13,7 @@ class Serie extends Model
 {
     protected $table = 'series';
 
+    // Relationships
     public function genre() {
         return $this->belongsTo(Genre::class);
     }
@@ -23,6 +24,62 @@ class Serie extends Model
 
     public function character(){
         return $this->hasMany(Character::class);
+    }
+
+    // Getters
+    public function getId(): int
+    {
+        return $this->getAttribute('id');
+    }
+
+    public function getOriginalId(): int
+    {
+        return $this->getAttribute('original_id');
+    }
+
+    public function getGenreId(): int
+    {
+        return $this->getAttribute('genre_id');
+    }
+
+    public function getName(): string
+    {
+        return $this->getAttribute('name');
+    }
+
+    public function getDescription(): string
+    {
+        return $this->getAttribute('description');
+    }
+
+    public function getPosterPath(): string
+    {
+        return $this->getAttribute('poster_path');
+    }
+
+    public function getTrailerLink(): ?string
+    {
+        return $this->getAttribute('trailer_link');
+    }
+
+    public function getTotalEpisodes(): int
+    {
+        return $this->getAttribute('total_episodes');
+    }
+
+    public function getReleaseDate(): int
+    {
+        return $this->getAttribute('release_date');
+    }
+
+    public function getPuntuation(): ?string
+    {
+        return $this->getAttribute('puntuation');
+    }
+
+    public function getDuration(): int
+    {
+        return $this->getAttribute('duration');
     }
 
 }
