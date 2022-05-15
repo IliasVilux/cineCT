68764b72d70cd9e8a47687cdfb531b52228631ba
elena.montiel.7e4 <elena.montiel.7e4@itb.cat>
2022-05-15 23:54:25 +0200

    Botón subir

--- resources/views/general/headerFooter.blade.php
+++ resources/views/general/headerFooter.blade.php
@@ -140,7 +140,9 @@
         @yield('content')
     </main>
     <!-- END MAIN -->
-
+    <button type="button" class="btn btn-floating btn-md" id="btn-back-to-top">
+        <i class="fas fa-arrow-up"></i>
+    </button>
     <!-- START FOOTER -->
     <footer class="footer text-center text-white">
         <div class="container-fluid">
@@ -197,5 +199,31 @@
     </footer>
     <!-- END FOOTER -->
 </body>
+<script>
+    let mybutton = document.getElementById("btn-back-to-top");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+    scrollFunction();
+    };
+
+    function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+    }
+    // When the user clicks on the button, scroll to the top of the document
+    mybutton.addEventListener("click", backToTop);
+
+    function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    }
+</script>
 
 </html>
