fcf12bf29f0b639ab09ceac6ef7f446f92019eae
judDam <judith.martinez.7e2@itb.cat>
2022-05-16 00:13:11 +0200

    Session with lang and top clean

--- resources/views/includes/session.blade.php
+++ resources/views/includes/session.blade.php
@@ -1,11 +1,10 @@
 <div class="card text-center bg-dark session-card">
     <div class="card-body">
         <img class="rounded-circle shadow-1-strong me-3 my-3" src="{{ Auth::user()->image->path }}" alt="a" width="125"height="125">
-        <p class="card-text">Holaa <strong>{{ Auth::user()->name }}</strong>, actualmente ya tienes tu session iniciada, qué
-            quieres hacer?</p>
+        <p class="card-text">{{ trans('warnings.hi') }}<strong>{{ Auth::user()->name }}</strong>{{ trans('warnings.open_session') }}</p>
         <div class="d-flex flex-column">
-            <a href="{{ route('home') }}" class="btn btn-violet mb-2">Volver</a>
-            <a href="{{ route('signout.user') }}" class="btn btn-violet">Cerrar sessión</a>
+            <a href="{{ route('home') }}" class="btn btn-violet mb-2">{{ trans('warnings.back') }}</a>
+            <a href="{{ route('signout.user') }}" class="btn btn-violet">{{ trans('warnings.close_session') }}</a>
         </div>
     </div>
 </div>
