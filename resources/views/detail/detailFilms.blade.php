e78974240dedf0827f8afb31ca20d79fbc00ade8
elena.montiel.7e4 <elena.montiel.7e4@itb.cat>
2022-05-15 22:56:52 +0200

    Arreglar card

--- resources/views/detail/detailFilms.blade.php
+++ resources/views/detail/detailFilms.blade.php
@@ -41,7 +41,7 @@
                 <img src="{{ $film->poster_path }}" class="img-thumbnail col-6 col-md-5 col-lg-4 mb-md-0" alt="Img {{ $film->name }}">
             @endif
 
-            <article class="col-6 more-info bg-dark p-3 ms-1" id="datasheet">
+            <article class="col-6 more-info bg-dark p-3 ms-1 ms-sm-0" id="datasheet">
                 <div class="d-none align-content-center flex-wrap d-sm-flex">
                     <h5 class="pe-2 fw-bold">{{ trans('detail.genre') }}:</h5>
                     <p>{{ $film->genre->name }}</p>
