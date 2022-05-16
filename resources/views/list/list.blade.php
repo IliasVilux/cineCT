caa209bf77c3d89c6315ee9b8e4025de39f81e06
elena <elena.montiel.7e4@itb.cat>
2022-05-16 16:46:57 +0200

    Ultimos cambios

--- resources/views/list/list.blade.php
+++ resources/views/list/list.blade.php
@@ -2,12 +2,7 @@
 @section('content')
 
 <head>
-    <link rel="stylesheet" href="{{asset('css/detail.css')}}">
-    <style>
-        .cursor{
-            cursor: pointer;
-        }
-    </style>
+    <link rel="stylesheet" href="{{asset('css/.css')}}">
 </head>
 
 <section class="container">
@@ -15,20 +10,25 @@
 <h3>Para crear tu lista de favoritos necessitas estar logueado, <a href="{{route('register.user')}}">INICIA SESSIÓN</a></h3>
 
 @else
-    <h1>MIS LISTAS</h1>
     <p class="d-none">{{$cont = 0}}</p>
+    <div class="container-fluid d-flex justify-content-between align-items-center">
+            <h1>MIS LISTAS</h1>
+            <a href="{{ url('/content/contentAnimes') }}" class="btn button-purple my-4" title="Back">
+            {{ trans('titles.back') }}
+            </a>
+        </div>
     @foreach($userFavs as $favList)
         <div>
             @if($favList->top_list == 1)
-                <a href="lista-fav/{{$favList->id}}">
-                    <div class="mt-4 p-5 w-100 bg-dark text-white rounded button-gold cursor">
-                        <h1>{{$favList->name}}</h1>
+                <a href="lista-fav/{{$favList->id}}" class="image-link col-4 col-md-3 col-lg-2 p-2 search-content-info">
+                    <div class="mt-4 p-2 w-100 bg-dark text-white rounded button-gold cursor">
+                        <h4>{{$favList->name}}</h4>
                     </div>  
                 </a>
             @else
                 <a href="lista-fav/{{$favList->id}}">
-                    <div class="mt-4 p-5 w-100 bg-dark text-white rounded button-list cursor">
-                        <h1>{{$favList->name}}</h1>
+                    <div class="mt-4 p-2 w-100 bg-dark text-white rounded button-list cursor">
+                        <h4>{{$favList->name}}</h4>
                     </div>
                 </a>
             @endif
@@ -36,10 +36,6 @@
         </div>
         <p class="d-none">{{$cont++}}</p>
     @endforeach
-
-    <a href="{{ url('/') }}" class="btn button-purple" title="Home">
-        Home
-    </a>
 
     @endif
 </section>
